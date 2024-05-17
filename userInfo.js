ed84a855318a6b95e2f7fc04c526814c2657b7d4
Kadia-D <kddellor@gmail.com>
2024-05-17 12:59:03 +0000

    Task 3 (edited)

--- userInfo.js
+++ userInfo.js
@@ -1,21 +1,3 @@
-/*const { formatArrayStrings } = require("./arrayManipulation");
-
-function createUserProfiles(originalNames, modifiedNames) {
-    return originalNames.map((name, index) =>{
-        return {
-            id: index+1,
-            originalName: name,
-            modifiedName: modifiedNames[index]
-        };
-    });
-}
-
-//Example
-const originalNames = ["Naya", "Afia", "Rahina", "Gerty", "Kay"];
-const modifiedNames = formatArrayStrings(originalNames)
-const userProfiles = createUserProfiles(originalNames, modifiedNames, id)
-console.log(userProfiles);*/
-
 const { formatArrayStrings } = require("./arrayManipulation");
 
 function createUserProfiles(originalNames, modifiedNames) {
