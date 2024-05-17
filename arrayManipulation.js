ebe634992e27fa5d8e4d6ab49b788621d505ad5e
Kadia-D <kddellor@gmail.com>
2024-05-17 11:13:54 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -12,4 +12,27 @@ function processArray(arr) {
 }
 
 //Example
-console.log(processArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(processArray(numbers))
+
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        const num =numbers[index];
+        if (num % 2 ===0) {
+            //capitaize strings with even corresponding numbers
+            return str.toUpperCase();
+        }
+        else{
+            return str.toLowerCase();
+        }
+    });
+}
+
+//Example 2
+const strings = ["Hello", "World", "I", "am", "glad", "to", "be","here", "with", "you"]
+console.log(formatArrayStrings(strings,numbers))
+
+module.exports = {
+    formatArrayStrings
+};
